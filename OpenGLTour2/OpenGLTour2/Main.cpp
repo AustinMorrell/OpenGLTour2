6fcc635eee863ba7bc45bf128e3be42dff6b54bb
Austin Morrell <austinlm19@gmail.com>
2016-08-16 17:23:18 -0500

    Set up finished

--- OpenGLTour2/OpenGLTour2/Main.cpp
+++ OpenGLTour2/OpenGLTour2/Main.cpp
@@ -41,21 +41,39 @@ int main()
 	auto minor = ogl_GetMinorVersion();
 	printf("GL: %i.%i\n", major, minor);
 
+	Gizmos::create();
+	mat4 view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0, 1, 0));
+	mat4 projection = glm::perspective(glm::pi<float>() * 0.25f, 16 / 9.f, 0.1f, 1000.f);
 	// After checking, the rest of the code goes here.
 
 	glClearColor(0.25f, 0.25f, 0.25f, 1);
 	glEnable(GL_DEPTH_TEST); // enables the depth buffer
 
+	//-----------------------------------------------------------------------------------------------------
 	while (glfwWindowShouldClose(window) == false && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
 	{
 		// Updates, logic, and render code starts here!
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		Gizmos::clear();
+		Gizmos::addTransform(glm::mat4(1));
+		vec4 white(1);
+		vec4 black(0, 0, 0, 1);
+
+		for (int i = 0; i < 21; i++)
+		{
+			Gizmos::addLine(vec3(-10 + i, 0, 10), vec3(-10 + i, 0, -10), i == 10 ? white : black);
+			Gizmos::addLine(vec3(10, 0, -10 + i), vec3(-10, 0, -10 + i), i == 10 ? white : black);
+		}
+
+		Gizmos::draw(projection * view);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+	//-----------------------------------------------------------------------------------------------------
 
+	Gizmos::destroy();
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
